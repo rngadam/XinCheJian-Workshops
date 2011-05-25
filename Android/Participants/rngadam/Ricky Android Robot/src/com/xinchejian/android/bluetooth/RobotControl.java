9d8a5aed17ee86aec6ec4791fcc3ee6ed7807e05
Ricky Ng-Adam <rngadam@yahoo.com>
2011-05-25 16:30:14 +0800

    Working project

---
+++ Android/Participants/rngadam/Ricky Android Robot/src/com/xinchejian/android/bluetooth/RobotControl.java
@@ -0,0 +1,58 @@
+package com.xinchejian.android.bluetooth;
+
+import java.io.IOException;
+import java.util.Formatter;
+import java.util.Locale;
+
+import android.util.Log;
+
+public class RobotControl {
+	private static final String TAG = RobotControl.class.getSimpleName();
+	public static final int SIDEWAYS_DEFAULT_POS = 90;
+	public static final int SIDEWAYS_MAX_POS = 160;
+	public static final int SIDEWAYS_MIN_POS = 20;
+	public static final int UPDOWN_DEFAULT_POS = 135;
+	public static final int UPDOWN_MAX_POS = 180;
+	public static final int UPDOWN_MIN_POS = 55;
+	public static final char UPDOWN_AXIS = 'U'; // up/down
+	public static final char SIDEWAYS_AXIS = 'S'; // left/right
+	private final StringBuilder formatted_output = new StringBuilder();	
+	private final Formatter formatter = new Formatter(formatted_output,
+			Locale.US);	
+	private final Bluetooth bluetooth;
+
+	public RobotControl(Bluetooth bluetooth) {
+		this.bluetooth = bluetooth;
+	}
+
+	public void sendCommandString(final char axis, final int value) {
+		sendCommandString(getCommandString(axis, value));
+	}
+	
+	private void sendCommandString(final String string) {
+		try {
+			byte[] msgBuffer;
+			Log.d(TAG, "sending: " + string);
+			msgBuffer = string.getBytes("ISO-8859-1");
+			if (msgBuffer.length != 7) {
+				throw new RuntimeException("Unexpected bytes output for: "
+						+ string);
+			}
+			synchronized(bluetooth) {
+				bluetooth.sendBuffer(msgBuffer);
+			}
+		} catch (final IOException e) {
+			Log.e(TAG, "Exception during write.", e);
+			return;
+		}
+	}
+	
+	private String getCommandString(final char axis, final int value) {
+		synchronized(formatter) {
+			formatter.format("%c0%03d0%d", axis, value, value % 9);
+			final String string = formatted_output.toString();
+			formatted_output.delete(0, formatted_output.length());
+			return string;
+		}
+	}
+}
--- Android/Participants/rngadam/Ricky Android Robot/src/com/xinchejian/android/rngadam/robot/RobotUserInterface.java
+++ Android/Participants/rngadam/Ricky Android Robot/src/com/xinchejian/android/rngadam/robot/RobotUserInterface.java
@@ -2,12 +2,96 @@ package com.xinchejian.android.rngadam.robot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.SeekBar;
+import android.widget.ToggleButton;
+
+import com.xinchejian.android.bluetooth.Bluetooth;
+import com.xinchejian.android.bluetooth.RobotControl;
 
 public class RobotUserInterface extends Activity {
-    /** Called when the activity is first created. */
+    private Bluetooth bluetooth;
+	private RobotControl robotControl;
+	private static final String arduinoAddress = "00:11:03:14:02:02";
+	
+	private ToggleButton bluetoothConnectionStatusToggle;
+	private ToggleButton deviceConnectedStatusToggle;
+	private int updownValue = RobotControl.UPDOWN_DEFAULT_POS;
+	private int sidewaysValue = RobotControl.SIDEWAYS_DEFAULT_POS;
+	
+	private final Handler handlerTimer = new Handler();
+	private final Runnable taskTimerUpdate = new Runnable() {
+		public void run() {
+			if(!bluetooth.isConnected()) {
+				bluetooth.connect();
+			} else {
+			    robotControl.sendCommandString(RobotControl.UPDOWN_AXIS, updownValue);
+			    robotControl.sendCommandString(RobotControl.SIDEWAYS_AXIS, sidewaysValue);
+			}
+			bluetoothConnectionStatusToggle.setChecked(bluetooth.checkBluetoothAvailable());
+			deviceConnectedStatusToggle.setChecked(bluetooth.isConnected());
+	        handlerTimer.postDelayed(this, 300);   			
+		}
+	};
+	private SeekBar upDownSeekBar;
+	private SeekBar sidewaysSeekBar;
+
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        bluetoothConnectionStatusToggle = (ToggleButton) findViewById(R.id.bluetoothConnectionStatus);
+        deviceConnectedStatusToggle = (ToggleButton) findViewById(R.id.bluetoothConnected);
+        upDownSeekBar = (SeekBar) findViewById(R.id.UpDownSeekBar);
+        sidewaysSeekBar = (SeekBar) findViewById(R.id.SidewaysSeekBar);
+        upDownSeekBar.setProgress(updownValue);
+        sidewaysSeekBar.setProgress(sidewaysValue);
+        upDownSeekBar.setMax(RobotControl.UPDOWN_MAX_POS);
+        sidewaysSeekBar.setMax(RobotControl.SIDEWAYS_MAX_POS);
+        upDownSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			public void onProgressChanged(final SeekBar seekBar,
+					final int progress, final boolean fromUser) {
+				if (fromUser) {
+					if (progress < RobotControl.UPDOWN_MIN_POS) {
+						updownValue = RobotControl.UPDOWN_MIN_POS;
+					} else {
+						updownValue = progress;
+					}
+				}
+			}
+
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+
+		});
+        sidewaysSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			public void onProgressChanged(final SeekBar seekBar,
+					final int progress, final boolean fromUser) {
+				if (fromUser) {
+					if (progress < RobotControl.SIDEWAYS_MIN_POS) {
+						sidewaysValue = RobotControl.SIDEWAYS_MIN_POS;
+					} else {
+						sidewaysValue = progress;
+					}
+				}
+			}
+
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+        });
+        
+        bluetooth = new Bluetooth(arduinoAddress);
+        robotControl = new RobotControl(bluetooth);
+		
+        handlerTimer.postDelayed(taskTimerUpdate, 300);        
     }
 }
