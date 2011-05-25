a3e12a6293212e7ad2384a86b9cb864eb702ae25
Paul Adams <paul@chaos-studio.com>
2011-05-25 23:17:49 +0800

    Change-Id: Idd2f4bcfc500df619c835ff9cd8d794a8fbf4eda

---
+++ Android/Participants/nihaopaul/android1/src/com/xinchejian/nihaopaul/a1/Android_one.java
@@ -0,0 +1,13 @@
+package com.xinchejian.nihaopaul.a1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Android_one extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
