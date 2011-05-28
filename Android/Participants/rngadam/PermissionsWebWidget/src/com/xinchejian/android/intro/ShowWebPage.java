3f4008e8343c0ae85f83bb3802366797cd203a6e
Ricky Ng-Adam <rngadam@yahoo.com>
2011-05-28 11:26:37 +0800

    more sample examples

---
+++ Android/Participants/rngadam/PermissionsWebWidget/src/com/xinchejian/android/intro/ShowWebPage.java
@@ -0,0 +1,19 @@
+package com.xinchejian.android.intro;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class ShowWebPage extends Activity {
+    private WebView webView;
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        webView = (WebView) findViewById(R.id.webkitWebView);
+        webView.loadUrl("file:/android_asset/helloworld.html");
+        //webView.loadUrl("http://www.google.com");
+    }
+}
---
+++ Android/Participants/rngadam/SampleListenerSeekBar/src/com/xinchejian/android/intro/Introduction.java
@@ -0,0 +1,33 @@
+package com.xinchejian.android.intro;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+public class Introduction extends Activity {
+    private EditText valueEditText;
+	private SeekBar progressSeekBar;
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        valueEditText = (EditText) findViewById(R.id.value);
+        progressSeekBar = (SeekBar) findViewById(R.id.progress);
+        progressSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+			
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+			
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				valueEditText.setText("Current: " + progress);
+			}
+		});
+    }
+}
